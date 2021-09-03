721e917d401135f3e92c164eb9efd6bc98f6158f
js02-castillo <jonnystuar@gmail.com>
2021-09-03 18:01:30 -0500

    add crud in RolController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// agregar controller
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RolController;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +27,9 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware'=>['auth']], function(){
+    Route::resource('roles',RolController::class);
+    Route::resource('usuarios',UsuarioController::class);
+    Route::resource('blogs',BlogController::class);
+});
