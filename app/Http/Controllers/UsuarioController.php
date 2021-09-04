14dc7b08f75119826c02fd047f47357d2465335d
js02-castillo <jonnystuar@gmail.com>
2021-09-04 15:12:12 -0500

    completed controller user and blogs

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -3,6 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//agregamos 
+use App\Http\Controllers\Controller;
+use Models\User;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Arr;
+
 
 class UsuarioController extends Controller
 {
@@ -13,7 +20,8 @@ class UsuarioController extends Controller
      */
     public function index()
     {
-        //
+        $usuarios=User::paginate(5);
+        return view('usuarios.index',compact('usuarios'));
     }
 
     /**
@@ -23,7 +31,8 @@ class UsuarioController extends Controller
      */
     public function create()
     {
-        //
+        $roles = Role::pluck('name','name')->all();
+        return view('usuarios.crear',compact('roles'));
     }
 
     /**
@@ -34,7 +43,22 @@ class UsuarioController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            'name'=>'required',
+            'email'=>'required|email|unique:users,email',
+            'password'=>'required|same:confirm-password',
+            'roles'=>'required'
+
+        ]);
+
+        $input=$request->all();
+        $input['password']=Hash::make($input['password']);
+
+        $user=User::create($input);
+        $user->assignRole($request->input('roles'));
+
+        return redirect()->route('usuarios.index');
+
     }
 
     /**
@@ -56,7 +80,12 @@ class UsuarioController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user= User::find($id);
+        $roles=Role::pluck('name','name')->all();
+        //agregar los parentesis a roles y probar si funciona ????
+        $userRole=$user->roles->pluck('name','name')->all();
+        return view('usuarios.editar',compact('user','roles','userRole'));
+
     }
 
     /**
@@ -68,7 +97,28 @@ class UsuarioController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request,[
+            'name'=>'required',
+            'email'=>'required|email|unique:users,email'.$id,
+            'password'=>'same:confirm-password',
+            'roles'=>'required'
+
+        ]);
+
+        $input = $request->all();
+        if(!empty($input['password'])){
+            $input['password']=Hash::make($input['password']);
+        }else{
+            $input = Arr::except($input,array('password'));
+        }
+
+        $user = User::find($id);
+        $user->update($input);
+        DB::table('model_has_roles')->where('model_id',$id)->delete();
+
+        $user->assignRole($request->input('roles'));
+        return redirect()->route('usuarios.index');
+
     }
 
     /**
@@ -79,6 +129,7 @@ class UsuarioController extends Controller
      */
     public function destroy($id)
     {
-        //
+        User::find($id)->delete();
+        return redirect()->route('usuarios.index');
     }
 }
